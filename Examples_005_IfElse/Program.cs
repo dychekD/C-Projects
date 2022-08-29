121d2db68c72a8f3c7a32584ae3c163d3fbcb695
Daria Dychek <dychek.d@gmail.com>
2022-08-29 14:02:29 +0200

    fifth programm

---
+++ Examples_005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Enter your username: ");
+string username = Console.ReadLine ();
+
+if (username.ToLower () == "maria")
+{
+    Console.WriteLine ("Nice to see you again, Maria!");
+}
+else
+{
+    Console.Write ("Hello, ");
+    Console. WriteLine (username);
+}
