46d538ea7d6252a73b599082450cedd6d089f1f5
Daria Dychek <dychek.d@gmail.com>
2022-08-31 22:58:57 +0200

    first array

---
+++ Arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {1, -3, 4, 5, 4, 7};
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine (index);   
+        break;           
+    }
+    index++;
+}
+
